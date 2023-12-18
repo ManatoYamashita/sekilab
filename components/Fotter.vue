7b2714782a7a6419bc9af08e44b25a2e411882cc
yamashita <g2172117@tcu.ac.jp>
2023-12-18 19:07:21 +0900

    [fix]footer icons

--- components/Fotter.vue
+++ components/Fotter.vue
@@ -16,28 +16,36 @@
                     </ul>
                 </div> -->
                 <div class="footer-middle">
-                    <h3>関研究室 -Information Security lab-</h3>
-                    <ul>
-                        <li><a href="mailto:seki@tcu.ac.jp">seki[at]tcu.ac.jp<small>（[at]を@に変えてください）</small></a></li>
-                        <!-- <li><a href="tel:+090-5506-5656">+090-5506-5656</a></li> -->
-                        <li><a href="https://www.comm.tcu.ac.jp/seki-lab">www.comm.tcu.ac.jp/seki-ken</a></li>
-                        <!-- <li><a href="https://www.comm.tcu.ac.jp/seki_lab/" target="_blank" rel="noopener noreferrer">旧サイトへ</a></li> -->
-                        <li>東京都市大学 横浜キャンパス3号館7階 3701</li>
-                    </ul>
+                    <h3>関研究室 <small>Information Security lab.</small></h3>
+                    <p>
+                        <a href="https://www.tcu.ac.jp/campuslife/introduction/yokohamacampus/">
+                        <span>東京都市大学 横浜キャンパス3号館7階 3701号室</span>
+                        </a>
+                        <ul class="info">
+                            <li><a href="mailto:seki@tcu.ac.jp">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" height="1.5rem" width="1.5rem" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+                            </a></li>
+                            <!-- <li><a href="tel:+090-5506-5656">+090-5506-5656</a></li> -->
+                            <li><a href="https://github.com/tcu-seki-lab">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" height="1.5rem" width="1.5rem" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
+                            </a></li>
+                        </ul>
+                    </p>
                 </div>
+
                 <div class="footer-right sitemap">
                     <h3>Sitemap</h3>
                     <ul>
-                        <li><nuxt-link to="/">Home</nuxt-link></li>
-                        <li><nuxt-link to="/about">About</nuxt-link></li>
-                        <li><nuxt-link to="/member">Member</nuxt-link></li>
-                        <li><nuxt-link to="/other">Other</nuxt-link></li>
+                        <li><nuxt-link to="/">ー Home</nuxt-link></li>
+                        <li><nuxt-link to="/about">ー About</nuxt-link></li>
+                        <li><nuxt-link to="/member">ー Member</nuxt-link></li>
+                        <li><nuxt-link to="/other">ー Other</nuxt-link></li>
                     </ul>
                 </div>
             </div>
             <div class="footer-flex flex-2">
                 <div class="univ footer-left">
-                    <h4><a href="https://www.tcu.ac.jp">東京都市大学 横浜キャンパス</a></h4>
+                    <h4><a href="https://www.tcu.ac.jp">東京都市大学</a></h4>
                     <p>
                         <a href="https://www.comm.tcu.ac.jp/gseis/department/index.html" target="_blank">環境情報学専攻</a><br>
                         <a href="https://informatics.tcu.ac.jp/faculty/system/" target="_blank">メディア情報学部 情報システム学科</a><br>
@@ -109,6 +117,15 @@
         width: auto;
         margin: 0 1rem;
     }
+    .info {
+        display: flex;
+        flex-direction: row;
+        padding: 0;
+        margin: 1rem ;
+    }
+    .info li {
+        margin: 0 .2rem;
+    }
     .univ {
         margin-top: 1rem;
         width: 100%;
@@ -138,7 +155,7 @@
     }
     p {
         font-size: 1rem;
-        margin-bottom: .5rem;
+        margin: .5rem 0;
         padding-left: 1rem;
     }
     .sitemap {
@@ -285,17 +302,24 @@
             display: flex;
             flex-direction: column;
         }
+        .footer-right, .footer-middle, .footer-left {
+            width: 100%;
+            margin: 0;
+        }
         h2 {
             font-size: 1.5rem;
         }
         h3 {
-            font-size: medium;
+            font-size: .9rem;
         }
         h4 {
             font-size: small;
         }
         ul, li, p {
             font-size: small;
+        }
+        li {
+            margin-left: .3rem;
         }
         .univ > p, .univ > a {
             font-size: x-small;
