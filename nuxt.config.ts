6c4f0be4aaa4d7454180fe42d609db36b8238db4
yamashita <g2172117@tcu.ac.jp>
2023-12-19 20:36:28 +0900

    [add]seo setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,24 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width',
+      viewport: 'width=device-width, initial-scale=1',
       title: 'Tokyo City University SEKI - Information Security Lab.',
+      meta: [
+          // 全てのオーガニック検索から除外
+          { name: 'robots', content: 'noindex, nofollow' },
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          { name: 'format-detection', content: 'telephone=no' },
+          { name: 'keywords', content: '東京都市大学, 関研究室, 情報セキュリティ, 関良明, 都市大, TCU, sekilab, seki, デザインデータ科学' },
+          { name: 'description', content: '"つながる"にもっと安心を。東京都市大学 情報システム学科の関研究室のホームページです。情報セキュリティを中心に、情報共有システムや情報セキュリティ行動支援システムの研究を行っています。' },
+          { property: 'twitter:card, content: summary_large_image' },
+          { property: 'og:title', content: 'Tokyo City University SEKI-Information Security Lab.' },
+          { property: 'og:description', content: 'a Website SEKI Lab. at TCU, research about Information Security.' },
+          { property: 'og:image', content: '/ogp.png' },
+          { property: 'og:type', content: 'website' },
+          { property: 'og:site_name', content: '東京都市大学 関研究室' },
+          { property: 'og:url', content: 'https://comm.tcu.ac.jp/seki_lab' },
+      ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
       ],
