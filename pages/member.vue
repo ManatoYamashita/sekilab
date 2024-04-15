87d5e1091ba6eff0810c0597e888bd8786b330ba
yamashita <g2172117@tcu.ac.jp>
2024-04-15 14:14:46 +0900

    [fix]image path

--- pages/member.vue
+++ pages/member.vue
@@ -62,41 +62,41 @@ import { reactive } from 'vue'
 const data = 'メンバー紹介'
 
 const membersM2 = reactive<{name: string, image: string, roll: string, detail: string}[]>([
-{name: "相吉澤 奏", image: "_nuxt/assets/mems/pre/aiyoshizawa.jpg", roll: "環境", detail: "「共分散構造分析の設計手法に関する研究」"},
+{name: "相吉澤 奏", image: "./assets/mems/pre/aiyoshizawa.jpg", roll: "環境", detail: "「共分散構造分析の設計手法に関する研究」"},
 ])
 const membersM1 = reactive<{name: string, image: string, roll: string, detail: string}[]>([
 {name: "not yet assigned", image: "", roll: "", detail: ""},
 ])
 const membersU4 = reactive<{name: string, image: string, roll: string, detail: string}[]>([
-  {name: "岸 佑奈", image: "_nuxt/assets/mems/2021/kishi.webp", roll: "ゼミ長", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "山下 真和都", image: "_nuxt/assets/mems/2021/yamashita.webp", roll: "広報", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "小西川 龍希", image: "_nuxt/assets/mems/2021/konishikawa.webp", roll: "イベント", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "小林 祥瑛", image: "_nuxt/assets/mems/2021/kobayashi-shoei.webp", roll: "イベント", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "新實 一心", image: "_nuxt/assets/mems/2021/niimi.webp", roll: "イベント", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "小林 将大", image: "_nuxt/assets/mems/2021/kobayashi-shouta.webp", roll: "環境", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "渡邊 悠人", image: "_nuxt/assets/mems/2021/watanabe.webp", roll: "環境", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "赤間 弦", image: "_nuxt/assets/mems/2021/akama.webp", roll: "広報", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "助川 優香", image: "_nuxt/assets/mems/2021/sukegawa.webp", roll: "懇親", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "若山 樹", image: "_nuxt/assets/mems/2021/wakayama.webp", roll: "懇親", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "五十嵐 星哉", image: "_nuxt/assets/mems/2021/igarashi.webp", roll: "合宿", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "深町 公貴", image: "_nuxt/assets/mems/2021/fukamachi.webp", roll: "合宿", detail: "「期間限定販売法における転売防止手法に関する研究」"},
-  {name: "行友 裕哉", image: "_nuxt/assets/mems/2021/yukitomo.webp", roll: "合宿", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "岸 佑奈", image: "./assets/mems/2021/kishi.webp", roll: "ゼミ長", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "山下 真和都", image: "./assets/mems/2021/yamashita.webp", roll: "広報", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "小西川 龍希", image: "./assets/mems/2021/konishikawa.webp", roll: "イベント", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "小林 祥瑛", image: "./assets/mems/2021/kobayashi-shoei.webp", roll: "イベント", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "新實 一心", image: "./assets/mems/2021/niimi.webp", roll: "イベント", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "小林 将大", image: "./assets/mems/2021/kobayashi-shouta.webp", roll: "環境", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "渡邊 悠人", image: "./assets/mems/2021/watanabe.webp", roll: "環境", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "赤間 弦", image: "./assets/mems/2021/akama.webp", roll: "広報", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "助川 優香", image: "./assets/mems/2021/sukegawa.webp", roll: "懇親", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "若山 樹", image: "./assets/mems/2021/wakayama.webp", roll: "懇親", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "五十嵐 星哉", image: "./assets/mems/2021/igarashi.webp", roll: "合宿", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "深町 公貴", image: "./assets/mems/2021/fukamachi.webp", roll: "合宿", detail: "「期間限定販売法における転売防止手法に関する研究」"},
+  {name: "行友 裕哉", image: "./assets/mems/2021/yukitomo.webp", roll: "合宿", detail: "「期間限定販売法における転売防止手法に関する研究」"},
 ])
 const membersU3 = reactive<{name: string, image: string, roll: string, detail: string}[]>([
 {name: "not yet assigned", image: "", roll: "", detail: ""},
 
-//   {name: "岸 祐奈", image: "_nuxt/assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
-//   {name: "山下 真和都", image: "_nuxt/assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
-//   {name: "岸 祐奈", image: "_nuxt/assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
-//   {name: "山下 真和都", image: "_nuxt/assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
-//   {name: "岸 祐奈", image: "_nuxt/assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
-//   {name: "山下 真和都", image: "_nuxt/assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
-//   {name: "岸 祐奈", image: "_nuxt/assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
-//   {name: "山下 真和都", image: "_nuxt/assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
-//   {name: "岸 祐奈", image: "_nuxt/assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
-//   {name: "山下 真和都", image: "_nuxt/assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
-//   {name: "岸 祐奈", image: "_nuxt/assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
-//   {name: "山下 真和都", image: "_nuxt/assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
+//   {name: "岸 祐奈", image: "./assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
+//   {name: "山下 真和都", image: "./assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
+//   {name: "岸 祐奈", image: "./assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
+//   {name: "山下 真和都", image: "./assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
+//   {name: "岸 祐奈", image: "./assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
+//   {name: "山下 真和都", image: "./assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
+//   {name: "岸 祐奈", image: "./assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
+//   {name: "山下 真和都", image: "./assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
+//   {name: "岸 祐奈", image: "./assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
+//   {name: "山下 真和都", image: "./assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
+//   {name: "岸 祐奈", image: "./assets/mems/m01.webp", roll: "ゼミ長", detail: "「AI生成イラストの判断」"},
+//   {name: "山下 真和都", image: "./assets/mems/m01.webp", roll: "広報", detail: "「未定」"},
 
 ])
 const membersOBOG = reactive<{name: string, detail: string}[]>([
